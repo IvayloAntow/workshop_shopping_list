e78c6e5c2259fdf82a5ba0473ca75172d94fe1b4
IvayloAntow <antow111111@gmail.com>
2023-02-22 22:13:53 +0200

    add product logic and html

--- src/main/java/com/example/prep/web/ProductController.java
+++ src/main/java/com/example/prep/web/ProductController.java
@@ -1,15 +1,44 @@
 package com.example.prep.web;
 
+import com.example.prep.model.binding.ProductAddBindingModel;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/products")
 public class ProductController {
 
      @GetMapping("/add")
-     public String add(){
+     public String add(Model model){
+
+          if(!model.containsAttribute("productAddBindingModel")){
+               model.addAttribute("productAddBindingModel", new ProductAddBindingModel());
+
+          }
+
           return "product-add";
      }
+
+     @PostMapping("/add")
+     public String addConfirm(@Valid ProductAddBindingModel productAddBindingModel,
+                              BindingResult bindingResult,
+                              RedirectAttributes redirectAttributes){
+
+          if(bindingResult.hasErrors()){
+               redirectAttributes.addFlashAttribute("productAddBindingModel", productAddBindingModel);
+               redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.productAddBindingModel");
+
+               return "return:add";
+          }
+
+               return "redirect:/" ;
+     }
+
+     // ToDo save in Db
 }
